dabc5fed70e7698d94ae88de4565c7ddbd8d359b
Ati <ativassileva@gmail.com>
2025-02-28 10:03:06 +0200

    Created MVC project

---
+++ WashWise/WashWise.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WashWise.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
